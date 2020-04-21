520169af92be5e207a8c44c29244f4b5b82f88e4
Minh Tuấn Bùi <buiminhtuan1507@gmail.com>
2020-04-21 20:21:48 +0700

    First commit

---
+++ routes/Users.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const users = express.Router()
+const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const User = require('../models/User')
+users.use(cors())
+
+process.env.SECRET_KEY = 'secret'
+
+users.post('/register', (req, res) => {
+  const today = new Date()
+  const userData = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email,
+    password: req.body.password,
+    created: today
+  }
+
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  })
+    //TODO bcrypt
+    .then(user => {
+      if (!user) {
+       User.create(userData)
+            .then(user => {
+              res.json({ status: user.email + 'Registered!' })
+            })
+            .catch(err => {
+              res.send('error: ' + err)
+            })
+        }
+      else {
+        res.json({ error: 'User already exists' })
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+
+users.post('/login', (req, res) => {
+  User.findOne({
+    where: {
+      email: req.body.email,
+      password :req.body.password
+    }
+  })
+    .then(user => {
+      if (user) {
+        //res.send("login success");
+        res.json(user);
+        }
+       else {
+        res.status(400).json({ error: 'User does not exist' })
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ error: err })
+    })
+})
+
+users.get('/profile', (req, res) => {
+ 
+
+  User.findOne({
+    where: {
+      id: req.body.id
+    }
+  })
+    .then(user => {
+      if (user) {
+        res.json(user);
+      } else {
+        res.send('User does not exist')
+      }
+    })
+    .catch(err => {
+      res.send('error: ' + err)
+    })
+})
+
+module.exports = users
